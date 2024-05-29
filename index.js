65da5cd67e57d6672dcbdf3ed32034a0e13a06ee
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-05-29 19:46:23 +0500

    conditional ternary operator in typescript

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+// Conditional Ternary Operator
+// Example 1
+let a = 10;
+let b;
+//     ifblock: elseblock
+b = ((a > 20) ? true : false);
+console.log(a);
+console.log(b);
+// Example 2
+let result = true;
+let finalResult = result ? "onsite" : "online";
+console.log(`Congratulations! Now You Are ${finalResult} Student in GIAIC`);
---
+++ index.ts
@@ -0,0 +1,25 @@
+// Conditional Ternary Operator
+// syntax 
+
+// condition? statement1 : statement2
+/*condition true print statement 1
+ condition false print statement2*/
+
+// Example 1
+
+let a: number = 10;
+let b: boolean;
+//         ifblock: elseblock
+b = a > 20 ? true : false;
+
+console.log(a);
+
+console.log(b);
+
+// Example 2
+
+let result: boolean = true;
+
+let finalResult = result ? "onsite" : "online";
+
+console.log(`Congratulations! Now You Are ${finalResult} Student in GIAIC`);
